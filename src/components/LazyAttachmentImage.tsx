893d459ba60cff669048c98ca8809fce51d2422f
Matheus Rodrigues <mathcivilce@gmail.com>
2025-06-11 20:31:45 +0800

    Fixed oppacity of image zoom click panel

--- src/components/LazyAttachmentImage.tsx
+++ src/components/LazyAttachmentImage.tsx
@@ -325,7 +325,7 @@ export const LazyAttachmentImage: React.FC<LazyAttachmentImageProps> = ({
 
     const modalContent = (
       <div 
-        className="fixed inset-0 bg-black bg-opacity-95 flex items-center justify-center"
+        className="fixed inset-0 bg-black bg-opacity-70 flex items-center justify-center"
         style={{ 
           zIndex: 999999,
           margin: 0, 
